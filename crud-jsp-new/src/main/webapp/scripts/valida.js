e92e66efa74fd5b146485bd39d5393714e737087
=joseamerico <=doichejunior@gmail.com>
2024-10-26 07:56:35 -0300

    validacao do formulario adiciona-usuario.html

--- crud-jsp-new/src/main/webapp/scripts/valida.js
+++ crud-jsp-new/src/main/webapp/scripts/valida.js
@@ -1,22 +1,50 @@
-function validar(){
+function valida() {
 
-    let login = frmUsuario.login.value
-    let role = frmUsuario.role.value
-    let password = frmUsuario.password.value
-     alert("ola")
-    if(login === ""){
-        alert("Preencha o campo login!")
-        frmUsuario.login.focus();
-        return false;
-    } else if (password === ""){
-    alert("Informe sua senha!")
-        frmUsuario.password.focus();
-        return false;
-    } else{
-        document.forms["frmContato"].submit();
 
+    let form = document.forms["meu-form"];
+    let login = document.forms["meu-form"]["login"].value;
+    let password = document.forms["meu-form"]["password"].value;
+    let password1 = document.forms["meu-form"]["password1"].value;
+
+
+
+    if (login === "") {
+        alert("Preencha o campo email!");
+        form.login.style.cssText = "border:solid 1px red;";
+        form.login.focus()
+        return false;
     }
 
+    if (password === "") {
+        alert("Preencha o campo password!");
+        //form.password.style.cssText = "border:solid 1px red;";
+        form.password.focus()
+        return false;
+    }
+    else if (password1 === "") {
+        alert("Preencha o campo confirm!");
+        form.password1.style.cssText = "border:solid 1px red;";
+        form.password1.focus()
+        return false;
+    }
+
+    else if(password!=password1){
+        alert("As senhas informadas não conferem!");
+        form.password1.style.cssText = "border:solid 1px red;";
+        return false;
+    }
+
+
+    else if (password === password1) {
+        
+        document.getElementById("img-confirm").src = "./imagens/chck.png";
+        form.login.style.cssText = "border:solid 1px green;";
+        form.password.style.cssText = "border:solid 1px green;";
+        form.password1.style.cssText = "border:solid 1px green;";
+        form.action  = 'controladora?action=AdicionaUsuario';
+        document.getElementById("meu-form").submit();
+        return true;
+    }
 
 
 }
