0876cb99c46aadbe1a520777ba208509defa647e
Jose Americo <doichejunior@gmail.com>
2024-11-12 15:44:17 -0300

    adicionando jar bin3

--- crud-jsp-new/src/main/java/servlet/ControladoraService.java
+++ crud-jsp-new/src/main/java/servlet/ControladoraService.java
@@ -1,6 +1,8 @@
 package servlet;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -68,7 +70,46 @@ public class ControladoraService extends HttpServlet {
 			new LoginApiHidrobike().doPost(req,resp);
 		}
 		else if (acao.equals("imprimir")){
-			new Impressao().doPost(req,resp);
+			//new Impressao().doPost(req,resp);
+			System.out.println("IMPRIMINDO...");
+			StringBuilder saida = new StringBuilder();
+			BufferedReader leitor;
+			ProcessBuilder processos;
+
+			String livro = req.getParameter("livro");
+			String folha = req.getParameter("folha");
+			String cidade = req.getParameter("cidade");
+			String funcionario = req.getParameter("funcionario");
+			String funcao = req.getParameter("funcao");
+			String motivo = req.getParameter("motivo");
+			String data = req.getParameter("data");
+			System.out.println("\"texto\"");
+
+			 String etiqueta ="                   CANCELAMENTO DO TERMO \\n \\n"
+					+ "CERTIFICO QUE FICA SEM EFEITOS A PÁGINA DO LIVRO N. " + livro + " FOLHA N. "
+					+ folha + ": MOTIVO:  " + motivo + ". " + "\\n EU " + funcionario + ", " + funcao
+					+ "(a) CONFERI E ASSINO.\\n" + cidade + " " + data;
+             System.out.println(etiqueta);
+
+			Process processo;
+			//String comandos ="java -jar C:\\Users\\jose.junior\\Desktop\\Exec\\bin3.jar \""+texto+"\"" ;
+			String comandos ="java -jar bin3.jar \""+etiqueta+"\"" ;
+
+			try {
+				processos = new ProcessBuilder("cmd.exe", "/c", String.join(" && ", comandos));
+				processo = processos.start();
+				processo.waitFor();
+				leitor = new BufferedReader(new InputStreamReader(processo.getInputStream()));
+
+				String linha = "";
+
+				while ((linha = leitor.readLine()) != null) {
+					saida.append(linha).append("\n");
+				}
+			} catch (IOException | InterruptedException ex) {
+				System.out.println(ex.getCause());
+			}
+
 		}
 
 
