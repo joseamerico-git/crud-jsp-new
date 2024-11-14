d744231fa972f81c6031cbffc2afd9f175704a36
=joseamerico <=doichejunior@gmail.com>
2024-11-13 22:26:30 -0300

    utilizando classes genericas java

--- crud-jsp-new/src/main/java/api/util/GenericClassJsonUtil.java
+++ crud-jsp-new/src/main/java/api/util/GenericClassJsonUtil.java
@@ -1,10 +1,35 @@
 package api.util;
 
+import com.google.gson.Gson;
+import model.Usuario;
+import model.veiculo.CodigoCorDenatran;
+import model.veiculo.Veiculo;
+
 public class GenericClassJsonUtil {
     public static <T> void printArray(T[] array) {
         for (T element : array) {
             System.out.println(element);
         }
+    }
+
+    public static <T> String convertJson(T Object) {
+
+        Gson gson = new Gson();
+        String json = gson.toJson(Object);
+        java.lang.String json1 = json;
+
+        return json1;
+
+    }
+
+    public static <T> String convertClassEmJson(T Objetct) {
+
+        Gson gson = new Gson();
+        String json = gson.toJson(Objetct);
+        java.lang.String json1 = json;
+        System.out.print(json1);
+        return json1;
+
     }
 
     public static void main(String[] args) {
@@ -14,5 +39,20 @@ public class GenericClassJsonUtil {
 
         printArray(intArray);  // Funciona com Integer
         printArray(stringArray);  // Funciona com String
+        Usuario usuario = new Usuario("teste","123","1");
+
+       System.out.println("convertendo " + convertJson(usuario));
+
+        Veiculo veiculo = new Veiculo();
+        veiculo.setAnoFabricacao(1997);
+        veiculo.setCodigoCor(CodigoCorDenatran.AZUL.getDescricao());
+        veiculo.setPesoBruto(2000.00);
+        veiculo.setPesoLiquido(1000.00);
+        veiculo.setCapacidadeTracao(111);
+       
+
+        System.out.println("convertendo " + convertJson(veiculo));
+
+
     }
 }
